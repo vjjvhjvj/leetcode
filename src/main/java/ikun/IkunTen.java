01d860cf6e305e83fd95f9c599fc3c285fcd94a9
niganma <2308528850@qq.com>
2023-04-16 19:48:23 +0800

    二叉树的最大深度

---
+++ src/main/java/ikun/IkunTen.java
@@ -0,0 +1,16 @@
+package ikun;
+
+public class IkunTen {
+    public static void main(String[] args) {
+
+    }
+    public int maxDepth(TreeNode root) {
+        if(root==null) {
+            return 0;
+        }else{
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left,right);
+        }
+    }
+}
